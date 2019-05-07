111d9b040d66f9f5e0fcd998eb1176c3233c5070
nahuel062 <nambroa@gmail.com>
2019-05-07 19:22:34 -0300

    hola

---
+++ app/src/main/java/com/example/applunacrowdfunding/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.applunacrowdfunding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.example.applunacrowdfunding.Conexion.ApiError;
+import com.example.applunacrowdfunding.Conexion.ApiInterface;
+import com.example.applunacrowdfunding.Conexion.Respuesta;
+import com.example.applunacrowdfunding.Conexion.RespuestaParametro;
+import com.example.applunacrowdfunding.Conexion.conexion;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final ApiInterface apiService = conexion.getClient().create(ApiInterface.class);
+
+        Call<Respuesta> call = apiService.multiplicidad(200);
+        call.enqueue(new Callback<Respuesta>() {
+            @Override
+            public void onResponse(Call<Respuesta> call, Response<Respuesta> response) {
+                if (!response.isSuccessful()) {
+                    String error = "Ha ocurrido un error. Contacte al administrador";
+                    if (response.errorBody()
+                            .contentType()
+                            .subtype()
+                            .equals("json")) {
+                        ApiError apiError = ApiError.fromResponseBody(response.errorBody());
+
+                        error = apiError.getMessage();
+                        Log.d("LoginActivity", apiError.getDeveloperMessage());
+                    } else {
+                        try {
+                            // Reportar causas de error no relacionado con la API
+                            Log.d("LoginActivity", response.errorBody().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+                    return;
+
+
+                }
+
+                /*Este es con call respuesta, para mostrar lista de usuarios
+                JsonArray arregloUsers = response.body().getMessage();
+               for(int i=0; i<arregloUsers.size(); i++){
+                    JsonObject aux = arregloUsers.get(i).getAsJsonObject();
+                    String nombre= aux.get("Nombre").getAsString();
+
+                }
+             String nombre = arregloUsers.get(0).getAsJsonObject().get("Nombre").getAsString();
+             EditText txtNombre= findViewById(R.id.txtNombre);
+                txtNombre.setText(nombre);*/
+                JsonArray numero = response.body().getMessage();
+                String nombre=numero.get(0).getAsJsonObject().get("numerito").getAsString();
+                EditText txtNombre= findViewById(R.id.txtNombre);
+                txtNombre.setText(nombre);
+
+            }
+            @Override
+            public void onFailure(Call<Respuesta> call, Throwable t) {
+                Log.d("LoginActivity", t.getMessage());
+
+
+            }
+        });
+    }
+}
